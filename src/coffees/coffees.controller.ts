95142c2a89fd50dc09145b55e9435c5793f3f365
Volodymyr Opanasiuk <opanasiukvlad@gmail.com>
2023-02-19 18:04:16 +0200

    initial commit

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Patch,
+  Delete,
+  Query,
+} from '@nestjs/common';
+
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
+  @Get()
+  findAll(@Query() paginationQuery) {
+    // const { limit, offset } = paginationQuery;
+    return this.coffeesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.coffeesService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() CreateCoffeeDto: CreateCoffeeDto) {
+    return this.coffeesService.create(CreateCoffeeDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() UpdateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, UpdateCoffeeDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.coffeesService.remove(id);
+  }
+}
